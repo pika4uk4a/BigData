cca4848078f1dedf59cf69603e4a99e6fd88fcd0
Крижановский Данил Романович <Danil.Krizhanovsky@mvideo.ru>
2025-12-24 13:27:03 +0300

    лабораторная работа #1

---
+++ BigDataSnowflake-main/migrations/03_dml_load_mock_data.sql
@@ -0,0 +1,21 @@
+-- DML: Загрузка данных из CSV файлов в таблицу mock_data
+COPY mock_data FROM '/data/MOCK_DATA.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY mock_data FROM '/data/MOCK_DATA (1).csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY mock_data FROM '/data/MOCK_DATA (2).csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY mock_data FROM '/data/MOCK_DATA (3).csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY mock_data FROM '/data/MOCK_DATA (4).csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY mock_data FROM '/data/MOCK_DATA (5).csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY mock_data FROM '/data/MOCK_DATA (6).csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY mock_data FROM '/data/MOCK_DATA (7).csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY mock_data FROM '/data/MOCK_DATA (8).csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY mock_data FROM '/data/MOCK_DATA (9).csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
